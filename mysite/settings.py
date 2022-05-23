d4fe9a601a36d2d88b84695f1fb272bf890d81ad
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-05-23 13:19:35 +0000

    email setup 2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 LOGIN_URL = "accounts/login"
 LOGIN_REDIRECT_URL = "index"
