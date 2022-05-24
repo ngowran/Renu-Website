1ecb464b95477cc794eb9347caa75daa86851513
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-05-24 18:36:27 +0000

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,10 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, "static"),
-]
+if DEBUG:
+  STATICFILES_DIRS = [
+          os.path.join(BASE_DIR, 'static')
+       ]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#STATICFILES_DIRS = [
+#  os.path.join(BASE_DIR, "static"),
+#]
 ALLOWED_HOSTS = ["*"]
 X_FRAME_OPTIONS = "*"
 
